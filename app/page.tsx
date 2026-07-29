356415d58155e0326414730b14f0ae2133d1e396
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-29 19:35:53 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
+import Dashboard from "../components/Dashboard";
+
 export default function Home() {
-  return (
-    <main style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      fontFamily: "Arial"
-    }}>
-      <h1>Infinity Trading AI Web 🚀</h1>
-    </main>
-  );
+  return <Dashboard />;
 }
