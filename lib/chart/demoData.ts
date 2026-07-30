89d16adf948fd7bef0bafd1b64719695405ca482
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 15:29:57 +0530

    Create demoData.ts

---
+++ lib/chart/demoData.ts
@@ -0,0 +1,50 @@
+import { Candle } from "./types";
+
+export const demoCandles: Candle[] = [
+
+  {
+    time: "2026-07-01",
+    open: 25000,
+    high: 25050,
+    low: 24980,
+    close: 25030,
+    volume: 1200,
+  },
+
+  {
+    time: "2026-07-02",
+    open: 25030,
+    high: 25120,
+    low: 25010,
+    close: 25090,
+    volume: 1800,
+  },
+
+  {
+    time: "2026-07-03",
+    open: 25090,
+    high: 25160,
+    low: 25070,
+    close: 25140,
+    volume: 2100,
+  },
+
+  {
+    time: "2026-07-04",
+    open: 25140,
+    high: 25210,
+    low: 25100,
+    close: 25190,
+    volume: 2400,
+  },
+
+  {
+    time: "2026-07-05",
+    open: 25190,
+    high: 25280,
+    low: 25170,
+    close: 25260,
+    volume: 3000,
+  }
+
+];
