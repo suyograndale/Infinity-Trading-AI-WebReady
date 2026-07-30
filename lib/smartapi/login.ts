b79355128884ba76ef313b2cc5cacfb0fb495e9b
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 19:21:44 +0530

    Update login.ts

--- lib/smartapi/login.ts
+++ lib/smartapi/login.ts
@@ -1,6 +1,11 @@
 import { SmartLoginResponse } from "./types";
+import { defaultAuth } from "./auth";
 
 export async function smartLogin(): Promise<SmartLoginResponse> {
+
+  console.log("Preparing SmartAPI Login...");
+
+  defaultAuth.isLoggedIn = false;
 
   return {
 
