8d8a4a873cbde485a6d51d6690b34097dbc55408
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-31 03:46:40 +0530

    Update login.ts

--- lib/smartapi/login.ts
+++ lib/smartapi/login.ts
@@ -1,8 +1,7 @@
-import { saveSession } from "./session";
+import { saveSession, clearSession } from "./session";
 import { createLoginRequest } from "./request";
 import { SmartLoginResponse } from "./types";
 import { defaultAuth } from "./auth";
-import { clearSession } from "./session";
 import { smartApiPost } from "./http";
 
 export async function smartLogin(): Promise<SmartLoginResponse> {
@@ -17,17 +16,23 @@ export async function smartLogin(): Promise<SmartLoginResponse> {
 
   console.log(request);
 
+  const response = await smartApiPost(request);
+
+  console.log(response);
+
   if (!response.status) {
-  throw new Error(response.message || "SmartAPI Login Failed");
-}
-saveSession(
-  response.data.jwtToken,
-  response.data.refreshToken,
-  response.data.feedToken
-);
-return {
-  jwtToken: response.data.jwtToken,
-  refreshToken: response.data.refreshToken,
-  feedToken: response.data.feedToken,
-};
+    throw new Error(response.message || "SmartAPI Login Failed");
+  }
+
+  saveSession(
+    response.data.jwtToken,
+    response.data.refreshToken,
+    response.data.feedToken
+  );
+
+  return {
+    jwtToken: response.data.jwtToken,
+    refreshToken: response.data.refreshToken,
+    feedToken: response.data.feedToken,
+  };
 }
