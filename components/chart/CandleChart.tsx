e18156a7ddca9c4c693640f899f413e6e466f973
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 15:36:12 +0530

    Create CandleChart.tsx

---
+++ components/chart/CandleChart.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+export default function CandleChart() {
+
+  return (
+
+    <div
+      style={{
+        width: "100%",
+        height: 500,
+        background: "#0f172a",
+        border: "1px solid #374151",
+        borderRadius: 10,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        fontSize: 24,
+      }}
+    >
+
+      📊 Candlestick Chart Container
+
+    </div>
+
+  );
+
+}
