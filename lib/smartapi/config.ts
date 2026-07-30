167cc0913c4f49c8427d320b1ed6fc53040e0105
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 19:30:47 +0530

    Update config.ts

--- lib/smartapi/config.ts
+++ lib/smartapi/config.ts
@@ -1,9 +1,6 @@
 export const SmartAPIConfig = {
-
   apiKey: process.env.NEXT_PUBLIC_SMARTAPI_API_KEY || "",
-
   clientCode: process.env.NEXT_PUBLIC_SMARTAPI_CLIENT_CODE || "",
-
-  totpSecret: process.env.SMARTAPI_TOTP_SECRET || "",
-
+  password: process.env.SMARTAPI_PASSWORD || "",
+  totp: process.env.SMARTAPI_TOTP || "",
 };
