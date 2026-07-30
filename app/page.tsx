345d7a70d1260e6ee4aa7021a91cd93b7d5dc109
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 12:51:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Sidebar from "@/components/dashboard/Sidebar";
+import MarketOverview from "@/components/dashboard/MarketOverview";
+import Watchlist from "@/components/dashboard/Watchlist";
+import TopGainers from "@/components/dashboard/TopGainers";
+import TopLosers from "@/components/dashboard/TopLosers";
+import EconomicCalendar from "@/components/dashboard/EconomicCalendar";
+import NewsPanel from "@/components/dashboard/NewsPanel";
+import FiiDiiPanel from "@/components/dashboard/FiiDiiPanel";
+
+import AIAnalysis from "@/components/AIAnalysis";
+import AISignalEngine from "@/components/AISignalEngine";
+import ICTPanel from "@/components/ICTPanel";
 import MarketOverview from "@/components/MarketOverview";
 import ICTPanel from "@/components/ICTPanel";
 import AISignalEngine from "@/components/AISignalEngine";
@@ -6,12 +18,83 @@ import Dashboard from "@/components/Dashboard";
 
 export default function Home() {
   return (
-    <>
-      <Dashboard />
-      <AIAnalysis />
-      <AISignalEngine />
-      <ICTPanel />
-      <MarketOverview />
-    </>
-  );
+
+<div
+style={{
+display:"flex",
+background:"#0f172a",
+minHeight:"100vh"
+}}
+>
+
+<Sidebar/>
+
+<div
+style={{
+flex:1,
+padding:20
+}}
+>
+
+<MarketOverview/>
+
+<div
+style={{
+display:"grid",
+gridTemplateColumns:"2fr 1fr",
+gap:20,
+marginTop:20
+}}
+>
+
+<div>
+
+<AISignalEngine/>
+
+<AIAnalysis/>
+
+<ICTPanel/>
+
+</div>
+
+<div>
+
+<Watchlist/>
+
+<div style={{marginTop:20}}>
+<TopGainers/>
+</div>
+
+<div style={{marginTop:20}}>
+<TopLosers/>
+</div>
+
+<div style={{marginTop:20}}>
+<FiiDiiPanel/>
+</div>
+
+</div>
+
+</div>
+
+<div
+style={{
+display:"grid",
+gridTemplateColumns:"1fr 1fr",
+gap:20,
+marginTop:20
+}}
+>
+
+<NewsPanel/>
+
+<EconomicCalendar/>
+
+</div>
+
+</div>
+
+</div>
+
+);
 }
