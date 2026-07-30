e4b1c77f974084754e26374c78e2b7f649d611d1
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 20:39:27 +0530

    Update service.ts

--- lib/smartapi/service.ts
+++ lib/smartapi/service.ts
@@ -1,6 +1,8 @@
 import { SmartAPIEndpoints } from "./api";
 import { smartAPI } from "./client";
 import { smartLogin } from "./login";
+import { session } from "./session";
+
 
 export async function initializeSmartAPI() {
 
@@ -9,7 +11,8 @@ export async function initializeSmartAPI() {
   const login = await smartLogin();
 console.log(SmartAPIEndpoints.baseUrl);
   console.log("SmartAPI Ready");
-
+console.log("Logged In :", session.loggedIn);
+  console.log("Authentication Layer Ready");
   return {
 
     config,
@@ -19,4 +22,4 @@ console.log(SmartAPIEndpoints.baseUrl);
   };
 
 }
-console.log("Authentication Layer Ready");
+
