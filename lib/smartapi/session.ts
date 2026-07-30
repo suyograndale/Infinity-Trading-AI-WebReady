eecd963e0a90adc4e500afded1f0823a0879835d
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 20:44:54 +0530

    Update session.ts

--- lib/smartapi/session.ts
+++ lib/smartapi/session.ts
@@ -1,23 +1,31 @@
 export interface SmartSession {
-
   jwtToken: string;
-
   refreshToken: string;
-
   feedToken: string;
-
   loggedIn: boolean;
-
 }
 
 export const session: SmartSession = {
-
   jwtToken: "",
-
   refreshToken: "",
-
   feedToken: "",
-
   loggedIn: false,
-
 };
+
+export function saveSession(
+  jwtToken: string,
+  refreshToken: string,
+  feedToken: string
+) {
+  session.jwtToken = jwtToken;
+  session.refreshToken = refreshToken;
+  session.feedToken = feedToken;
+  session.loggedIn = true;
+}
+
+export function clearSession() {
+  session.jwtToken = "";
+  session.refreshToken = "";
+  session.feedToken = "";
+  session.loggedIn = false;
+}
