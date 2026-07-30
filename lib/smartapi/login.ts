2d537b9d39d57a72bed0c288f1e2823b1d061e02
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 20:52:26 +0530

    Update login.ts

--- lib/smartapi/login.ts
+++ lib/smartapi/login.ts
@@ -1,28 +1,28 @@
 import { createLoginRequest } from "./request";
 import { SmartLoginResponse } from "./types";
 import { defaultAuth } from "./auth";
-import { session } from "./session";
+import { clearSession } from "./session";
 import { smartApiPost } from "./http";
+
 export async function smartLogin(): Promise<SmartLoginResponse> {
 
   console.log("Preparing SmartAPI Login...");
 
   defaultAuth.isLoggedIn = false;
-const request = createLoginRequest();
 
-console.log(request);
-  session.loggedIn = false;
+  clearSession();
+
+  const request = createLoginRequest();
+
+  console.log(request);
+
   const response = await smartApiPost(request);
 
-console.log(response);
+  console.log(response);
+
   return {
-
     jwtToken: "",
-
     refreshToken: "",
-
     feedToken: "",
-
   };
-
 }
