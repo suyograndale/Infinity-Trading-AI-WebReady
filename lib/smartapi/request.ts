69e352cd8c3edefbab27b2eabc49fb9170660f40
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 20:51:54 +0530

    Update request.ts

--- lib/smartapi/request.ts
+++ lib/smartapi/request.ts
@@ -1,17 +1,10 @@
 import { SmartAPIConfig } from "./config";
 
 export function createLoginRequest() {
-
   return {
-
     clientcode: SmartAPIConfig.clientCode,
-
     password: SmartAPIConfig.password,
-
     totp: SmartAPIConfig.totp,
-
     state: "live",
-
   };
-
 }
