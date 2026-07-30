1f97b2a6508c4693b3e59c23ec018910d099ede4
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-31 04:40:35 +0530

    Update config.ts

--- lib/smartapi/config.ts
+++ lib/smartapi/config.ts
@@ -1,9 +1,9 @@
 export const SmartAPIConfig = {
-  apiKey: process.env.NEXT_PUBLIC_SMARTAPI_API_KEY || "",
-  clientCode: process.env.NEXT_PUBLIC_SMARTAPI_CLIENT_CODE || "",
-  password: process.env.SMARTAPI_PASSWORD || "",
-  totp: process.env.SMARTAPI_TOTP || "",
-  ipAddress: process.env.NEXT_PUBLIC_CLIENT_PUBLIC_IP || "",
-localIp: process.env.NEXT_PUBLIC_CLIENT_LOCAL_IP || "",
-macAddress: process.env.NEXT_PUBLIC_MAC_ADDRESS || "",
+  apiKey: process.env.NEXT_PUBLIC_SMARTAPI_API_KEY || "******",
+  clientCode: process.env.NEXT_PUBLIC_SMARTAPI_CLIENT_CODE || "******",
+  password: process.env.SMARTAPI_PASSWORD || "******",
+  totp: process.env.SMARTAPI_TOTP || "******",
+  ipAddress: process.env.NEXT_PUBLIC_CLIENT_PUBLIC_IP || "******",
+localIp: process.env.NEXT_PUBLIC_CLIENT_LOCAL_IP || "******",
+macAddress: process.env.NEXT_PUBLIC_MAC_ADDRESS || "******",
 };
