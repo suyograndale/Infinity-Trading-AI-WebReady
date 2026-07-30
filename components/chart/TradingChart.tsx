bc7a791f411f74a4481066356ab160d2f577e730
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 13:55:46 +0530

    Create TradingChart.tsx

---
+++ components/chart/TradingChart.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import {
+  createChart,
+  ColorType,
+} from "lightweight-charts";
+
+export default function TradingChart() {
+
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    if (!chartRef.current) return;
+
+    const chart = createChart(chartRef.current, {
+
+      width: chartRef.current.clientWidth,
+
+      height: 500,
+
+      layout: {
+
+        background: {
+          type: ColorType.Solid,
+          color: "#111827",
+        },
+
+        textColor: "#ffffff",
+
+      },
+
+      grid: {
+
+        vertLines: {
+          color: "#374151",
+        },
+
+        horzLines: {
+          color: "#374151",
+        },
+
+      },
+
+    });
+
+    const candleSeries = chart.addSeries(
+      (await import("lightweight-charts")).CandlestickSeries
+    );
+
+    candleSeries.setData([
+      {
+        time: "2026-07-01",
+        open: 25000,
+        high: 25060,
+        low: 24980,
+        close: 25030,
+      },
+      {
+        time: "2026-07-02",
+        open: 25030,
+        high: 25120,
+        low: 25010,
+        close: 25090,
+      },
+      {
+        time: "2026-07-03",
+        open: 25090,
+        high: 25150,
+        low: 25070,
+        close: 25130,
+      },
+    ]);
+
+    chart.timeScale().fitContent();
+
+    return () => {
+
+      chart.remove();
+
+    };
+
+  }, []);
+
+  return (
+
+    <div
+
+      ref={chartRef}
+
+      style={{
+
+        width: "100%",
+
+        height: "500px",
+
+      }}
+
+    />
+
+  );
+
+}
