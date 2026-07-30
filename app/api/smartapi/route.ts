575f25bb5ff094fd384a5f5ee49487b073cc805e
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 20:40:23 +0530

    Update route.ts

--- app/api/smartapi/route.ts
+++ app/api/smartapi/route.ts
@@ -1,8 +1,29 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
-  return NextResponse.json({
-    success: true,
-    message: "SmartAPI Route Ready",
-  });
+export async function POST(req: NextRequest) {
+
+  try {
+
+    const body = await req.json();
+
+    return NextResponse.json({
+      success: true,
+      message: "SmartAPI Route Working",
+      request: body,
+    });
+
+  } catch {
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Invalid Request",
+      },
+      {
+        status: 400,
+      }
+    );
+
+  }
+
 }
