cf13a95229d6f54e35a8efc06034b61dc16147cc
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 11:58:23 +0530

    Update ICTPanel.tsx

--- components/ICTPanel.tsx
+++ components/ICTPanel.tsx
@@ -4,37 +4,33 @@ import { calculateICT } from "@/lib/ict/ictEngine";
 
 export default function ICTPanel() {
 
- const candles = [
+  const candles = [
+    {
+      open: 25000,
+      high: 25040,
+      low: 24980,
+      close: 25020,
+    },
+    {
+      open: 25020,
+      high: 25090,
+      low: 25010,
+      close: 25080,
+    },
+  ];
 
-  {
-    open:25000,
-    high:25040,
-    low:24980,
-    close:25020
-  },
-
-  {
-    open:25020,
-    high:25090,
-    low:25010,
-    close:25080
-  }
-
-];
-
-const ict = calculateICT(candles);
+  const ict = calculateICT(candles);
 
   return (
     <div
       style={{
-        background:"#1f2937",
-        padding:20,
-        borderRadius:15,
-        color:"white",
-        marginTop:25
+        background: "#1f2937",
+        padding: 20,
+        borderRadius: 15,
+        color: "white",
+        marginTop: 25,
       }}
     >
-
       <h2>🧠 ICT Analysis</h2>
 
       <p>BOS : {ict.bos ? "✅" : "❌"}</p>
@@ -46,8 +42,6 @@ const ict = calculateICT(candles);
       <p>Order Block : {ict.orderBlock ? "✅" : "❌"}</p>
 
       <p>Liquidity Sweep : {ict.liquiditySweep ? "✅" : "❌"}</p>
-
     </div>
   );
-
 }
