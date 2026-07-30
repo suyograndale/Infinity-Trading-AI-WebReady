9541b2c8ca4a973ceb446dca1d0f459223ed18c6
Suyog Narendra Randale <78803822+suyograndale@users.noreply.github.com>
2026-07-30 12:49:07 +0530

    Create FiiDiiPanel.tsx

---
+++ components/dashboard/FiiDiiPanel.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+export default function FiiDiiPanel(){
+
+return(
+
+<div
+
+style={{
+
+background:"#1f2937",
+
+padding:20,
+
+borderRadius:12,
+
+color:"white"
+
+}}
+
+>
+
+<h2>🏦 FII / DII Activity</h2>
+
+<h3
+
+style={{
+
+color:"#22c55e"
+
+}}
+
+>
+
+FII : +₹1,250 Cr
+
+</h3>
+
+<h3
+
+style={{
+
+color:"#3b82f6"
+
+}}
+
+>
+
+DII : +₹980 Cr
+
+</h3>
+
+</div>
+
+);
+
+}
